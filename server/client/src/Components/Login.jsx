31ae2e45823880a1094864ec96cf86b11dc6d5bd
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-09-26 15:02:34 -0500

    style properties

--- server/client/src/Components/Login.jsx
+++ server/client/src/Components/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
+const divStyle = {
+    margin: '40px',
+    border: '5px solid blue'
+  };
+
+const labelStyle = {
+    color: "blue"
+};
+
+
 const login = () => {
     return (
-        <div className="mdl-grid">
+        // <div style={divStyle}>
+        <div className="mdl-grid" style={divStyle}>
 
             <div class="mdl-cell mdl-cell--4-col"></div>
             <div class="mdl-cell mdl-cell--4-col">
@@ -13,24 +24,27 @@ const login = () => {
                     </div>
 
                     <br />
-                    <label>Enter your email</label>
+                    <label style={labelStyle}>Enter your email</label>
                     <br />
                     <input type="text" name="email" className="mdl-textfield__input" />
                     <br />
-                    <label>Enter your password</label>
+                    <label style={labelStyle}>Enter your password</label>
                     <br />
                     <input type="password" name="pass" className="mdl-textfield__input" />
                     <br />
-                    <div className="mdl-card__actions mdl-card--border">
+
+                    <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">Login</button>
+                    
+                    {/* <div className="mdl-card__actions mdl-card--border">
                         <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
                             Login
                        </a>
-                    </div>
+                    </div> */}
                 </div>
             </div>
             <div class="mdl-cell mdl-cell--4-col"></div>
         </div>
-
+        // </div>
     );
 }
 export default login;
